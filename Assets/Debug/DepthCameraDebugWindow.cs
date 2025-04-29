ce926ffe4cd492af2c48b7a3ae9c42725e06edea
adresa97 <adriresa97@gmail.com>
2025-04-29 20:55:45 +0200

    Game scene construction begins

--- Assets/Debug/DepthCameraDebugWindow.cs
+++ Assets/Debug/DepthCameraDebugWindow.cs
@@ -6,8 +6,10 @@ using UnityEngine.UIElements;
 public class DepthCameraDebugWindow : EditorWindow
 {
     private GameEvents depthCameraEvents;
+    private int lastGridSelection, newGridSelection;
+    private string[] worldOptions;
 
-    [MenuItem("Window/Depth Camera/Events Debug Window")]
+    [MenuItem("Window/Depth Camera/Depth Camera Debug Window")]
 
     public static void ShowExample()
     {
@@ -17,38 +19,71 @@ public class DepthCameraDebugWindow : EditorWindow
 
     public void OnEnable()
     {
+        worldOptions = new string[] {"Mundo Real", "Mundo Oculto"};
+        newGridSelection = (int)Shader.GetGlobalFloat("_IsDepthCameraPreview");
+        lastGridSelection = newGridSelection;
         depthCameraEvents = (GameEvents)Resources.FindObjectsOfTypeAll(typeof(GameEvents)).Where((obj) => obj.name == "FromPlayerManagerEvents").First();
     }
 
     public void OnGUI()
     {
-        if (!Application.isPlaying)
+        GUIStyle titleStyle = new GUIStyle(GUI.skin.label)
         {
-            EditorGUILayout.LabelField("Solo activo durante la ejecución");
-            return;
-        }
-
-        EditorGUILayout.LabelField("Selecciona el canal de eventos");
-        depthCameraEvents = (GameEvents)EditorGUILayout.ObjectField(depthCameraEvents, typeof(GameEvents), false);
+            alignment = TextAnchor.MiddleCenter,
+            fontSize = 14,
+            fontStyle = FontStyle.Bold
+        };
 
         GUILayout.Space(10);
 
-        EditorGUILayout.LabelField("Selecciona el evento a mandar");
-        if (GUILayout.Button("Activar Camara"))
+        if (!Application.isPlaying)
         {
-            if (depthCameraEvents != null) depthCameraEvents.Emit(new StopRecordingEvent());
+            EditorGUILayout.LabelField("Control de visualizador en el editor", titleStyle);
+
+            GUILayout.Space(10);
+
+            EditorGUILayout.LabelField("Selecciona el mundo a mostrar");
+            newGridSelection = GUILayout.SelectionGrid(lastGridSelection, worldOptions, 2);
+            if (newGridSelection != lastGridSelection)
+            {
+                Shader.SetGlobalFloat("_IsDepthCameraPreview", newGridSelection);
+                lastGridSelection = newGridSelection;
+            }
         }
-        if (GUILayout.Button("Desactivar Camara"))
+        else
         {
-            if (depthCameraEvents != null) depthCameraEvents.Emit(new ForceStopReplayEvent());
-        }
-        if (GUILayout.Button("Activar Preview"))
-        {
-            if (depthCameraEvents != null) depthCameraEvents.Emit(new InitiatePreviewEvent());
-        }
-        if (GUILayout.Button("Desactivar Preview"))
-        {
-            if (depthCameraEvents != null) depthCameraEvents.Emit(new CancelPreviewEvent());
+            EditorGUILayout.LabelField("Enviar eventos de la cámara en ejecución", titleStyle);
+
+            GUILayout.Space(10);
+
+            EditorGUILayout.LabelField("Selecciona el canal de eventos");
+            depthCameraEvents = (GameEvents)EditorGUILayout.ObjectField(depthCameraEvents, typeof(GameEvents), false);
+
+            GUILayout.Space(10);
+
+            EditorGUILayout.LabelField("Selecciona el evento a mandar");
+            if (GUILayout.Button("Activar Camara"))
+            {
+                if (depthCameraEvents != null) depthCameraEvents.Emit(new StopRecordingEvent());
+            }
+            if (GUILayout.Button("Desactivar Camara"))
+            {
+                if (depthCameraEvents != null) depthCameraEvents.Emit(new ForceStopReplayEvent());
+            }
+            if (GUILayout.Button("Activar Preview"))
+            {
+                if (depthCameraEvents != null) depthCameraEvents.Emit(new InitiatePreviewEvent());
+            }
+            if (GUILayout.Button("Desactivar Preview"))
+            {
+                if (depthCameraEvents != null) depthCameraEvents.Emit(new CancelPreviewEvent());
+            }
+
+            if (newGridSelection != 0)
+            {
+                newGridSelection = 0;
+                lastGridSelection = 0;
+            }
         }
     }
 }
